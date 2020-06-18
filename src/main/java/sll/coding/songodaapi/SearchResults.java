70cbb96d4fc8a8fd025e983a0bbf85597e7388d6
SLL <luisjk266@gmail.com>
2020-06-18 08:24:16 +0100

    W.I.P search result changes, getting next, prev, and last pages with a method instead of having to search again.

--- src/main/java/sll/coding/songodaapi/SearchResults.java
+++ src/main/java/sll/coding/songodaapi/SearchResults.java
@@ -1,6 +1,12 @@
 package sll.coding.songodaapi;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SearchResults {
 
@@ -54,6 +60,59 @@ public class SearchResults {
 
     public int getTotalResults() {
         return (int) ((long) meta.get("total"));
+    }
+
+    public SearchResults nextPage() throws IOException {
+        JSONObject response = get(getNextPageUrl());
+        assert response != null;
+        return new SearchResults((JSONObject) response.get("links"), (JSONObject) response.get("meta"));
+    }
+
+    static JSONObject get(String url) throws IOException {
+        URL url1 = new URL(url.replaceAll(" ", "%20"));
+        HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setRequestProperty("User-Agent", "PostmanRuntime/7.24.0");
+
+        if (connection.getResponseCode() != 200) {
+            throw new IOException("Failed: Error code " + connection.getResponseCode());
+        }
+
+        InputStream in = new BufferedInputStream(connection.getInputStream());
+        String output = convertStreamToString(in);
+
+        connection.disconnect();
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            return (JSONObject) parser.parse(output);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    protected static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 
 }
